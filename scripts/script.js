48ffa3d28322cd037c481380d9fb92887076253e
Sabine <sabine@21.lv>
2020-03-25 20:25:21 +0100

    start the game by clicking on the overlay and flip sound

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ class AudioController {
         this.matchSound = new Audio('assets/audio/matched.mp3');
         //this.levelUpSound = new Audio('assets/audio/level-up.mp3');
         this.victorySound = new Audio('assets/audio/victory.mp3');
-        //this.gameOver = new Audio('assets/audio/xxxxxxxx.mp3');
+        this.gameOver = new Audio('assets/audio/game-over.mp3');
         this.bgMusic.volume = 0.5;
         this.bgMusic.loop = true;
     }
@@ -34,21 +34,54 @@ class AudioController {
     }
 }
 
+// Cards
+
+class MixOrMatch {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+    startGame() {
+        this.cardToCheck = null; //gets called multiple times e.g. when restarting the game
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = []; //empty array
+        this.busy = true; //will put false once game is strated
+    }
+    flipCard(card) {
+        if (this.canFlipCard(card)) {
+            this.audioController.flip();
+        }
+    }
+    canFlipCard(card) {
+        return true;
+        //check if user is allowed to flip the card
+        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck //does not allow to flip cards that are already flipped, animating or matched
+        // creates a boolean = if thisnotbusy is false and does not include and card does not equal card to check will evaluate to true, because the statement is true
+        //so if it returns TRUE the player can flip a card
+    }
+}
+
 // Starts the game only when page is fully loaded
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-            // game.startGame();
+            overlay.classList.remove('visible'); //starts game whenever clicked on overlay
+            game.startGame();
         });
     });
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            // game.flipped(card);
+            game.flipCard(card); //whenever clicked on a card
         });
     });
 }
@@ -59,4 +92,4 @@ if (document.readyState === 'loading') { //loads js only after page is fully loa
     ready();
 }
 
-let audioController = new AudioController();
+// let audioController = new AudioController();
